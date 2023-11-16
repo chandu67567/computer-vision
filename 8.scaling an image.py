9a72c9329f01edf790c1db8821d582d471994467
chandu67567 <132892059+chandu67567@users.noreply.github.com>
2023-11-16 09:25:01 +0530

    Create 8.scaling an image.py

---
+++ 8.scaling an image.py
@@ -0,0 +1,11 @@
+import cv2
+img = cv2.imread("E:/Computer Vision/computer vision input and output/1.grayscaleinput.png")
+cv2.imshow("original image",img)
+cv2.waitKey(0)
+img = cv2.resize(img,(600,600))
+cv2.imshow("image",img)
+cv2.waitKey(0)
+img = cv2.resize(img,(100,100))
+cv2.imshow("image",img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
